04b7158644f1a16af11e12b8ae6c65e619feeaaf
Colt Thatcher <colt.thatcher303@gmail.com>
2022-02-11 10:47:02 -0800

    Add Header.js, code contents to the file

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import React from "react";
+
+
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h1>Kombucha Klub</h1>
+    </React.Fragment>
+  );
+}
+
+export default Header;
